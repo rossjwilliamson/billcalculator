e8451373b4dac79264661bc9696b96943f70a846
Ross Williamson <43893097+rossjwilliamson@users.noreply.github.com>
2018-10-30 19:46:29 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+let selector = (s) => {
+ 
+ return document.querySelector(s);
+
+}
+
+
+
+let btn = selector("#calculateBtn");
+
+btn.addEventListener("click",() => {
+
+
+
+let billAmount = selector("#billInput").value;
+let calculateTotal = selector("#calculateTotal").value;
+let billOutput = selector("#billOutput");
+
+
+
+
+if(billAmount!="" && calculateTotal!=""){
+
+	if(billAmount > 0 && calculateTotal > 0){
+
+
+
+let total = billAmount / calculateTotal;
+
+
+billOutput.innerHTML = `you need to tip £${total.toFixed(2)} each`;
+
+}else{
+	swal("OOPS","Please enter a number above 0 cheers","error");
+}
+}else{
+	swal("OOPS","Please fill in all the form fields","error");
+}
+
+  
+
+});
